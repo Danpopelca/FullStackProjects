279cfaf9e3d54c76331f65e751bf21f1a7a08c18
Danpopelca <119097088+Danpopelca@users.noreply.github.com>
2025-02-17 14:11:22 -0500

    Added home route to urls.py

--- PythonSectionMate/core/urls.py
+++ PythonSectionMate/core/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import assign_sections_view, save_server  # Asigură-te că save_server este importat
 
 urlpatterns = [
+    path("", assign_sections_view, name="home"),  # ✅ Aceasta va fi pagina principală
     path("assign/", assign_sections_view, name="assign_sections"),
     path("save_server/", save_server, name="save_server"),
 ]
