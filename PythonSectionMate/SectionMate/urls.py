5bfe2b839beea28a350ef3a873e971af382eae29
Danpopelca <119097088+Danpopelca@users.noreply.github.com>
2025-02-17 14:15:56 -0500

    Update urls.py

--- PythonSectionMate/SectionMate/urls.py
+++ PythonSectionMate/SectionMate/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import assign_sections_view, save_server  # Asigură-te că save_server este importat
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('core.urls')),  # Include rutele din `core`
+    path("", assign_sections_view, name="home"),  # ✅ Aceasta va fi pagina principală
+    path("assign/", assign_sections_view, name="assign_sections"),
+    path("save_server/", save_server, name="save_server"),
 ]
